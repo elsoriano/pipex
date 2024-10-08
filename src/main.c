273d0265078bf421a72c4cc74711a71b661cad1b
rhernand <rhernand@student.42malaga.com>
2024-10-08 18:08:16 +0200

    pruebas pipes

--- src/main.c
+++ src/main.c
@@ -6,17 +6,22 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:28:49 by rhernand          #+#    #+#             */
-/*   Updated: 2024/10/08 12:17:08 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:54:33 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
+	int	fd[2];
+	int	pid;
+	int	n;
+	int	buff;
 
 	if (!argv[0] || !argv)
 		return (1);
@@ -27,5 +32,25 @@ int	main(int argc, char **argv, char **envp)
 	{
 		printf("%s\n", envp[i]);
 		i++;
+	}
+	if (pipe(fd) == -1)
+		return (3);
+	pid = fork();
+	if (pid == -1)
+		return (3);
+	if (pid > 0)
+	{
+		printf("this is the parent process. PID = %d\n", pid);
+		close(fd[1]);
+		read(fd[0], &buff, sizeof(int));
+		printf("result of buff = %d\n", buff);
+		wait(NULL);
+	}
+	else
+	{
+		n = 20;
+		printf("this is the child process. PID = %d\n", pid);
+		close(fd[0]);
+		write(fd[1], &n, sizeof(int));
 	}
 }
