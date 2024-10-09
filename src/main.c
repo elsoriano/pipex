3f4e59bef28dbe3855a2c8412cbce2d0d64db6b1
Rodrigo Hernandez De Miguel <rhernand@c3r5s5.42malaga.com>
2024-10-09 19:29:31 +0200

    try dup2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rhernand <rhernand@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 16:28:49 by rhernand          #+#    #+#             */
-/*   Updated: 2024/10/08 17:54:33 by rhernand         ###   ########.fr       */
+/*   Updated: 2024/10/09 19:29:03 by rhernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
-	int	fd[2];
+	int	pipe[2];
 	int	pid;
 	int	n;
 	int	buff;
+	int	fd;
 
 	if (!argv[0] || !argv)
 		return (1);
@@ -33,7 +34,8 @@ int	main(int argc, char **argv, char **envp)
 		printf("%s\n", envp[i]);
 		i++;
 	}
-	if (pipe(fd) == -1)
+	fd = open(argv[1]);
+	if (pipe(pipe) == -1)
 		return (3);
 	pid = fork();
 	if (pid == -1)
@@ -41,8 +43,8 @@ int	main(int argc, char **argv, char **envp)
 	if (pid > 0)
 	{
 		printf("this is the parent process. PID = %d\n", pid);
-		close(fd[1]);
-		read(fd[0], &buff, sizeof(int));
+		close(pipe[1]);
+		read(pipe[0], &buff, sizeof(int));
 		printf("result of buff = %d\n", buff);
 		wait(NULL);
 	}
